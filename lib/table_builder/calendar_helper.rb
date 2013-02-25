6d4e47261f0c53085e7eae8f95037126014cdb88
Brett Eveson <brett@jettblue-designs.com>
2013-02-25 13:14:40 -0700

    add class to active calendar cells

--- lib/table_builder/calendar_helper.rb
+++ lib/table_builder/calendar_helper.rb
@@ -33,20 +33,21 @@ module CalendarHelper
       options = options_from_hash(args)
       day_method = options.delete(:day_method) || :date
       id_pattern = options.delete(:id)
+      
       tbody do
         @calendar.objects_for_days(@objects, day_method).to_a.sort{|a1, a2| a1.first <=> a2.first }.each do |o|
           key, array = o
           day, objects = array
           concat(tag(:tr, options, true)) if(day.wday ==  @calendar.first_weekday)
           if @row_header && day.wday ==  @calendar.first_weekday
-            row_header_options = td_options(day, id_pattern)
+            row_header_options = td_options(day, id_pattern, objects)
             row_header_options[:class] ||= ""
             row_header_options[:class] << " row_header"
             concat(tag(:td, row_header_options, true))
             yield(day, nil)
             concat("</td>")
           end
-          concat(tag(:td, td_options(day, id_pattern), true))
+          concat(tag(:td, td_options(day, id_pattern, objects), true))
           yield(day, objects)
           concat('</td>')
           concat('</tr>') if(day.wday ==  @calendar.last_weekday)
@@ -60,13 +61,14 @@ module CalendarHelper
       @calendar.objects_for_days(@objects)
     end
 
-    def td_options(day, id_pattern)
+    def td_options(day, id_pattern, objects)
       options = {}
       css_classes = []
       css_classes << 'today'    if day.strftime("%Y-%m-%d") ==  @today.strftime("%Y-%m-%d")
       css_classes << 'notmonth' if day.month != @calendar.month
       css_classes << 'weekend'  if day.wday == 0 or day.wday == 6
       css_classes << 'future'   if day > @today.to_date
+      css_classes << 'has_entries'  if not objects.empty?
       options[:class] = css_classes.join(' ') unless css_classes.empty?
       options[:id]    = day.strftime(id_pattern) if id_pattern
       options
--- test/calendar_helper_test.rb
+++ test/calendar_helper_test.rb
@@ -56,7 +56,7 @@ class CalendarHelperTest < ActionView::TestCase
         %(<tr><td class="notmonth weekend">(30)</td><td>(1)</td><td>(2)</td><td>(3)</td><td>(4)</td><td>(5)</td><td class="weekend">(6)</td></tr>) <<
         %(<tr><td class="weekend">(7)</td><td>(8)</td><td>(9)</td><td>(10)</td><td>(11)</td><td>(12)</td><td class="weekend">(13)</td></tr>) <<
         %(<tr><td class="weekend">(14)</td><td>(15)</td><td>(16)</td><td>(17)</td><td>(18)</td><td>(19)</td><td class="weekend">(20)</td></tr>) <<
-        %(<tr><td class="weekend">(21)</td><td>(22)</td><td>(23)</td><td>(24)</td><td>(25)</td><td>(26)34</td><td class="weekend">(27)</td></tr>) <<
+        %(<tr><td class="weekend">(21)</td><td>(22)</td><td>(23)</td><td>(24)</td><td>(25)</td><td class="has_entries">(26)34</td><td class="weekend">(27)</td></tr>) <<
         %(<tr><td class="weekend">(28)</td><td>(29)</td><td>(30)</td><td>(31)</td><td class="notmonth">(1)</td><td class="notmonth">(2)</td><td class="notmonth weekend">(3)</td></tr>) <<
         %(</tbody>) <<
       %(</table>)
@@ -167,7 +167,7 @@ class CalendarHelperTest < ActionView::TestCase
         %(<tr><td class="notmonth weekend">(30)</td><td>(1)</td><td>(2)</td><td>(3)</td><td>(4)</td><td>(5)</td><td class="weekend">(6)</td></tr>) <<
         %(<tr><td class="weekend">(7)</td><td>(8)</td><td>(9)</td><td>(10)</td><td>(11)</td><td>(12)</td><td class="weekend">(13)</td></tr>) <<
         %(<tr><td class="weekend">(14)</td><td>(15)</td><td>(16)</td><td>(17)</td><td>(18)</td><td>(19)</td><td class="weekend">(20)</td></tr>) <<
-        %(<tr><td class="weekend">(21)</td><td>(22)</td><td>(23)</td><td>(24)</td><td>(25)</td><td>(26)34</td><td class="weekend">(27)</td></tr>) <<
+        %(<tr><td class="weekend">(21)</td><td>(22)</td><td>(23)</td><td>(24)</td><td>(25)</td><td class="has_entries">(26)34</td><td class="weekend">(27)</td></tr>) <<
         %(<tr><td class="weekend">(28)</td><td>(29)</td><td>(30)</td><td>(31)</td><td class="notmonth">(1)</td><td class="notmonth">(2)</td><td class="notmonth weekend">(3)</td></tr>) <<
         %(</tbody>) <<
       %(</table>)
